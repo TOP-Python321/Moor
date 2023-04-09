faf79b6224f3984766d118d79aba1147feeec668
Schwarzepolizei <98344304+Schwarzepolizei@users.noreply.github.com>
2023-04-09 21:04:58 +0400

    Delete 1.2.py

--- 2023.04.09/1.2.py
+++
@@ -1,6 +0,0 @@
-num = int(input("Введите целое число: "))
-
-prev_num = num - 1
-next_num = num + 1
-
-print(f"Следующее за числом {num} число: {next_num} \nДля числа {num} предыдущее число: {prev_num}")
