1021f0929ab7aebafc2e1fdbf347b39cd9cd4fe4
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-04-21 14:19:22 +0400

    removed check greater than zero

--- 2023.04.16/1.py
+++ 2023.04.16/1.py
@@ -4,17 +4,17 @@ digit3 = input("Третье число: ")
 
 sum_of_positive = 0
     
-if digit1.isdecimal() > 0:
+if digit1.isdecimal():
     sum_of_positive += int(digit1)
 elif float(digit1) > 0:
     sum_of_positive += float(digit1)
     
-if digit2.isdecimal() > 0:
+if digit2.isdecimal():
     sum_of_positive += int(digit2)
 elif float(digit2) > 0:
     sum_of_positive += float(digit2)
     
-if digit3.isdecimal() > 0:
+if digit3.isdecimal():
     sum_of_positive += int(digit3)
 elif float(digit3) > 0:
     sum_of_positive += float(digit3)
