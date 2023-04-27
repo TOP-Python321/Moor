9de345e2e22a0aca5c8b602d7fb09b5272885e6a
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-04-27 11:34:52 +0400

    increased execution speed

--- 2023.04.23/4.py
+++ 2023.04.23/4.py
@@ -1,17 +1,44 @@
+# Оптимизировал скорость выполнения программы, так как в первом варианте очень долго считались числа, если разрядность была больше 3
+# через time посчитал, что скорость подсчета 4 разряда была 91 секунда, теперь - 0.002 секунды
+# использовал решето Эратосфена. Знаю, что не проходили это, просто решил попробовать этот метод.
+# ниже есть первый вариант.
+
 rank = int(input("Укажите разряд: "))
 
 max_number = int("9" * rank)
 min_number = (max_number + 1) // 10
 count = 0
 
-for num in range(min_number, max_number + 1): 
-    for i in range(2, num): 
-        if(num % i) == 0: 
-            break 
-    else: 
-        count += 1
+prime = [True] * (max_number + 1)
+prime[0] = prime[1] = False
+for i in range(2, max_number + 1):
+    if not prime[i]:
+        continue
+    for j in range(i * i, max_number + 1, i):
+        prime[j] = False
+primes = len([i for i in range(min_number, max_number + 1) if prime[i]])
+print(primes)
 
-print(count)        
+# Укажите разряд: 4
+# 1061
+
+
+# Изначальный вариант
+
+# rank = int(input("Укажите разряд: "))
+
+# max_number = int("9" * rank)
+# min_number = (max_number + 1) // 10
+# count = 0
+
+# for num in range(min_number, max_number + 1): 
+    # for i in range(2, num): 
+        # if(num % i) == 0: 
+            # break 
+    # else: 
+        # count += 1
+
+# print(count)        
 
 # Укажите разряд: 4
 # 1061
