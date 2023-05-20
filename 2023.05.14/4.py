c42ddc14694f3910cfedfb1b253fd03f85cd6ec8
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-05-20 18:31:59 +0400

    Added check if string length is greater than 2

--- 2023.05.14/4.py
+++ 2023.05.14/4.py
@@ -3,15 +3,25 @@ def countable_nouns(number: int, words: tuple[str, str, str]) -> str:
     Returns one word out of three passed as the second argument 'words'
     that matches the number passed as the first argument.
     """
-    numeral = str(number)[-1]
-    if int(numeral) == 1:
-        return words[0]
-    elif 2 <= int(numeral) <= 4:
-        return words[1]
+    numeral = str(number)
+    
+    if len(numeral) > 1:
+        if int(numeral[-1]) == 1 and int(numeral[-2]) != 1:
+            return words[0]
+        elif 2 <= int(numeral[-1]) <= 4 and 2 < int(numeral[-2]) < 1:
+            return words[1]
+        else:
+            return words[2]
     else:
-        return words[2]
-        
-        
+        if int(numeral[-1]) == 1:
+            return words[0]
+        elif 2 <= int(numeral[-1]):
+            return words[1]
+        else:
+            return words[2]
+            
+            
+            
 # >>> countable_nouns(2, ("год", "года", "лет"))
 # 'года'
 # >>> countable_nouns(10, ("год", "года", "лет"))
