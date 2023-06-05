8e9e52b9bb0b4af9b59e4b0ad4bedf5ec1981799
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-06-05 11:49:00 +0400

    Completed 2 task

---
+++ 2023.05.28/2.py
@@ -0,0 +1,28 @@
+from utils import important_message
+
+
+def main():
+    """
+    Функция выводит текст сообщения запрошенное от пользователя.
+    """
+    message = input('Текст сообщения: ')
+    important_message(message)
+    
+    return None
+# >>> main()
+# Текст сообщения: Python’s with statement supports the concept of a runtime context defined by a context manager. This is implemented using a pair of methods that allow user-defined classes to define a runtime context that is entered before the statement body is executed and exited when the statement ends:
+#=====================================================================================================================#
+#                                                                                                                     #
+#       Python’s with statement supports the concept of a runtime context defined by a context manager. This is       #
+#   implemented using a pair of methods that allow user-defined classes to define a runtime context that is entered   #
+#                      before the statement body is executed and exited when the statement ends:                      #
+#                                                                                                                     #
+#=====================================================================================================================#
+# >>>
+# >>> text = 'Достал нож - режь. Достал хлеб - ешь.'
+# >>> msg = important_message(text)
+#=====================================================================================================================#
+#                                                                                                                     #
+#                                        Достал нож - режь. Достал хлеб - ешь.                                        #
+#                                                                                                                     #
+#=====================================================================================================================#
