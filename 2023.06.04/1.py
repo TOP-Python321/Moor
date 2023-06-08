7e85b837c3a335693c7c2c899e0a7a23517f29d2
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-06-08 14:36:15 +0400

    Add comments

--- 2023.06.04/1.py
+++ 2023.06.04/1.py
@@ -14,3 +14,13 @@ def product(numbers: Iterable[float]) -> float:
     elif 0 in numbers:
         return 0.0
     return float(numbers[0]) * product(numbers[1:])
+    
+    
+# >>> product(range(10, 60, 10))
+# 12000000.0
+# >>>
+# >>> product([0.12, 0.05, -0.09, 0.0, 0.21])
+# 0.0
+# >>>
+# >>> product([0.12, 0.05, -0.09, 0.21, 1223, 523, 102])
+# -7398.4607172
