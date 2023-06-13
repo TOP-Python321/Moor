27bed50d17d3014e87bc219074df4042bec86419
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-06-13 14:43:31 +0400

    Fixes

--- 2023.05.14/4.py
+++ 2023.05.14/4.py
@@ -3,17 +3,15 @@ def countable_nouns(number: int, words: tuple[str, str, str]) -> str:
     Returns one word out of three passed as the second argument 'words'
     that matches the number passed as the first argument.
     """
-    if number % 100 in [11, 12, 13, 14]:
-        return words[2]
+    last_digit = number % 10
+    
+    if last_digit == 1 and number % 100 not in [11, 12, 13, 14]:
+        return words[0]
+    elif last_digit in [2, 3, 4] and number % 100 not in [11, 12, 13, 14]:
+        return words[1]
+    # ИСПРАВИТЬ: как бы вот этот блок else логически объединить с самым первым блоком if, в котором возвращается такое же значение? может и проверок получится поменьше
     else:
-        last_digit = number % 10
-        if last_digit == 1:
-            return words[0]
-        elif last_digit in [2, 3, 4]:
-            return words[1]
-        # ИСПРАВИТЬ: как бы вот этот блок else логически объединить с самым первым блоком if, в котором возвращается такое же значение? может и проверок получится поменьше
-        else:
-            return words[2]
+        return words[2]
 
 
 # >>> countable_nouns(2, ("год", "года", "лет"))
