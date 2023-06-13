104bdc3633cf12d23f671ec7f680cad1b9c27bd0
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-06-13 14:49:05 +0400

    Remove return

--- 2023.05.28/2.py
+++ 2023.05.28/2.py
@@ -1,14 +1,14 @@
 from utils import important_message
 
 
-def main():
+def main() -> None:
     """
     Функция выводит текст сообщения запрошенное от пользователя.
     """
     message = input('Текст сообщения: ')
     print(important_message(message))
-    
-    return None
+
+
 # >>> main()
 # Текст сообщения: Python’s with statement supports the concept of a runtime context defined by a context manager. This is implemented using a pair of methods that allow user-defined classes to define a runtime context that is entered before the statement body is executed and exited when the statement ends:
 #=====================================================================================================================#
