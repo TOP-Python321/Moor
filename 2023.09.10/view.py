3c8158389c928ef01cf7729f337a50309a6cc685
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-09-11 15:43:36 +0400

    Добавление модуля view

---
+++ 2023.09.10/view.py
@@ -0,0 +1,12 @@
+class CLI:
+    @staticmethod
+    def input_email() -> str:
+        return input('Введите email: ')
+
+    @staticmethod
+    def show_incorrect_message() -> None:
+        print('Введен некорректный email!')
+
+    @staticmethod
+    def show_save_message() -> None:
+        print('Email успешно сохранен.')
