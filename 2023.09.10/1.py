a689459bc45b2b2a123d9a6d9beb574f69d333cb
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-09-12 14:02:49 +0400

    Добавление точки входа

---
+++ 2023.09.10/1.py
@@ -0,0 +1,19 @@
+""" Точка входа. """
+
+from controller import Application
+
+
+def main():
+    app = Application()
+    app.run_app()
+
+
+if __name__ == '__main__':
+    main()
+
+
+# Введите email: asdasdaf@ioioi.com
+# Email успешно сохранен.
+# Введите email: asdofif
+# Введен некорректный email!
+# Введите email:
