99de5fd6f13d5c70127317f4907a7cd4af5091c3
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-09-12 14:02:26 +0400

    Добавление документации

--- 2023.09.10/controller.py
+++ 2023.09.10/controller.py
@@ -1,22 +1,28 @@
+""" Управляющий модуль. """
+
 from model import Email, FileIO
 from view import CLI
 
 
 class Application:
-    def __init__(self, email_model: Email):
-        self.email_model = email_model
+    """ Описывает взаимодействие с моделью и представлением. """
 
-    def save_email(self, email: str):
+    def __init__(self):
+        self.model_email: str
+
+    def save_email(self, email: str) -> None:
+        """Сохраняет переданный email адрес."""
         try:
-            self.email_model.email = email
+            self.model_email = Email(email)
             FileIO.add_email(email)
             CLI.show_save_message()
         except ValueError as ex:
             CLI.show_incorrect_message()
 
-    def run_app(self):
+    def run_app(self) -> None:
+        """Запускает условно-бесконечный цикл получения адресов от пользователя."""
         while True:
             email = CLI.input_email()
-            if email == '':
+            if email == "":
                 break
             self.save_email(email)
